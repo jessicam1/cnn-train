fb26074a39ec8fedcd80346daa92f5d4bd075100
Jessica Martin <jessica.martin@nih.gov>
2022-02-07 13:00:05 -0600

    gen_predictions.py added to pipeline

---
+++ gen_predictions.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+import sys
+import time
+import argparse
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from pathlib import Path
+from fast5fetch.fast5data import get_all_fast5s
+from fast5fetch.fast5data import train_test_val_split
+from fast5fetch.fast5data import readid_x_generator_many
+from fast5fetch.fast5data import readid_x_generator_many_wrapper
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description="get neural network predictions for a dataset")
+    parser.add_argument("-m", "--model",
+            help="neural network model to make predictions")
+    parser.add_argument("-l", "--library", type=str,
+            help="path to guppy library to get predictions for")
+    parser.add_argument("-w", "--window", type=int,
+            help="length of window to use when sampling raw signal")
+    parser.add_argument("-b", "--batchsize", default=32, type=int,
+            help="predictions batchsize; ensure same as batchsize model was trained")
+    # parser.add_argument("-s", "--savefile",
+    #         help="name of tabfile to save predictions to")
+    parser.add_argument("-t", "--threshold", default=0.5, type=float,
+            help="sigmoid threshold value")
+    parser.add_argument("-g", "--gpulim", default=4096, type=int,
+            help="gpu memory limit in mb")
+    # parser.add_argument("-n", "--numreads", type=int,
+    #       help="number of read-ids to load and get predictions for")
+    parser.add_argument("-v", "--verbose", action="store_true",
+            help="verbose model")
+    return parser.parse_args(args)
+
+def main():
+    args = parse_args(sys.argv[1:])
+    CPUS = 5
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        limit_gpu(gpus[0], 4096)
+
+    model = tf.keras.models.load_model(args.model)
+
+    gen_ds = build_ds_from_gen(args.library, args.window, args.batchsize, CPUS)
+    readids_and_predictions(gen_ds, model, args.batchsize, args.window)
+
+def limit_gpu(gpu_id, gpu_mem_lim):
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+                gpu_id,
+                [tf.config.experimental.VirtualDeviceConfiguration(
+                    memory_limit = gpu_mem_lim)])
+    except RuntimeError as err:
+        print(err)
+
+def build_ds_from_gen(library, window, batchsize, CPUS):
+    dirs = []
+    dirs += [library]
+    fast5s = get_all_fast5s(dirs)
+    gen_ds = tf.data.Dataset.from_generator(readid_x_generator_many_wrapper,
+            args=[fast5s, window, True, CPUS],
+            output_signature=(tf.TensorSpec(shape=(),
+                dtype=tf.string),
+                tf.TensorSpec(shape=(window,1), dtype=tf.float32)))# .take(90)
+
+
+    row = [0] * window
+    row_read = ["foo"]
+    dummy_rows = []
+    dummy_reads = []     
+    for i in range(batchsize):
+        dummy_rows = np.append(dummy_rows, row, axis=0)
+        dummy_reads.append(row_read)
+    dummy_rows_tf = tf.constant(dummy_rows, dtype=tf.float32,
+            shape=[batchsize, window, 1])
+    dummy_reads_tf = tf.constant(dummy_reads, dtype=tf.string,
+            shape=[batchsize,])
+    dummy_ds = tf.data.Dataset.from_tensor_slices((dummy_reads_tf, dummy_rows_tf))
+
+    gen_ds = gen_ds.concatenate(dummy_ds)
+
+    return gen_ds
+
+def readids_and_predictions(gen_ds, model, batchsize, window):
+    i = 0
+    # batch = []
+    batch = np.empty([32, window, 1])
+    readids = []
+    for readid, x in gen_ds:
+        if i < batchsize:
+            # batch = np.append(batch, x.numpy().reshape([1, window, 1]), axis=0)
+            batch[i,:] = x.numpy().reshape([1, window, 1])
+            i += 1
+            # batch = np.asarray(batch).reshape([1, window, 1]) 
+            readids.append(readid)
+        if i == batchsize:
+            preds = model.predict(batch)
+            for j in range(len(batch)-1): #range(len(batch)-1)
+                print("\t".join([readids[j].numpy().decode("utf-8"),
+                    str(preds[j][0])]))
+            batch = np.empty([32, window, 1])
+            # batch = []
+            readids = []
+            i = i - batchsize
+
+if __name__ == "__main__":
+    main()
+
+# gen_ds = build_ds_from_gen(args.library, args.window, CPUS)
+#
+# for readid, x in gen_ds:
+#     pred = model.predict(x)
+#     print(readid, pred)
--- pipeline.sh
+++ pipeline.sh
@@ -4,6 +4,8 @@ model="neuralnets/mycnn/models/test_4kseq_model/"
 mkdir -p "$model"
 logsdir="neuralnets/mycnn/tblogs/"
 mkdir -p "$logsdir"
+seqlength=4000
+batchsize=32
 pos_dirs=(
 	"exp/K562_5EU_1440_labeled_run/guppy/"
 	"exp/K562_5EU_1440_labeled_II_run/guppy/"
@@ -14,7 +16,28 @@ neg_dirs=(
 	"exp/K562_5EU_0_unlabeled_II_run/guppy/"
 	"exp/K562_5EU_0_unlabeled_III_run/guppy/"
 )
-seqlength=4000
+experiments=(
+	"K562_5EU_0_unlabeled_run"
+	"K562_5EU_0_unlabeled_II_run"
+	"K562_5EU_0_unlabeled_III_run"
+	"K562_5EU_1440_labeled_run"
+	"K562_5EU_1440_labeled_II_run"
+	"K562_5EU_1440_labeled_III_run"
+	"K562_5EU_60_labeled_heat_run"
+	"K562_5EU_60_labeled_heat_II_run"
+	"K562_5EU_60_labeled_heat_III_run"
+	"K562_5EU_60_labeled_heat_IV_run"
+	"K562_5EU_60_labeled_heat_V_run"
+	"K562_5EU_60_labeled_run"
+	"K562_5EU_60_labeled_II_run"
+	"K562_5EU_60_labeled_III_run"
+	"K562_5EU_60_labeled_IV_run"
+	"K562_5EU_60_labeled_V_run"
+	"K562_5EU_60_labeled_VI_run"
+	)
+predictionspath="/home/martinjl2/projects/ontml/analysis/predictions/results/$model"
+mkdir -p $predictionspath
+pospreds="pospreds.txt"
 
 echo ">>>CREATING MODEL<<<"
 python neuralnets/src/cnn_structure.py \
@@ -32,5 +55,37 @@ python neuralnets/src/train.py \
 	--window $seqlength \
 	--ratio 0.3 \
 	--threshold 0.5 \
-	--batchsize 32 \
+	--batchsize $batchsize \
 	--gpulim 4096
+
+echo ">>> GETTING PREDICTIONS FROM MODEL<<<" 
+echo -e "library\tpositive_predictions\ttotal_predictions" > $predictionspath/$pospreds
+
+for lib in "${experiments[@]}"
+do
+	echo ">>> MAKING PREDICTIONS DIRECTORY <<<"
+	mkdir -p $predictionspath/$lib
+	echo $predictionspath/$lib
+	echo ">>> GETTING PREDICTIONS FOR $lib <<<"
+	savefile="predictions.tab"
+	pospreds="pospreds.txt"
+	echo "predictions saved at $predictionspath/$savefile"
+	python neuralnets/src/gen_predictions.py \
+		--model $model \
+		--library $DATA_DIR/$lib/guppy/ \
+		--window $seqlength \
+		--batchsize $batchsize \
+		--threshold 0.5 \
+		--gpulim 4096 \
+		--verbose \
+	| grep -v 'foo' \
+	> $predictionspath/$lib/$savefile
+	
+	echo ">>> SUMMING POSITIVE PREDICTIONS FOR $lib <<<"
+	echo -en "$lib \t" >> $predictionspath/$pospreds
+	awk '{s+=$3}END{printf s "\t"}' $predictionspath/$lib/$savefile >> $predictionspath/$pospreds
+	echo "predictions summed in output file."
+	wc -l < $predictionspath/$lib/$savefile | awk '{print $1 -1}' >> $predictionspath/$pospreds 
+
+done
+wait
