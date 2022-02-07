07308196ae839ef6d6e9cef40cb6517630780734
Jessica Martin <jessica.martin@nih.gov>
2022-02-07 09:51:19 -0600

    train.py: cleaned up unused code, imports, linting

--- train.py
+++ train.py
@@ -5,15 +5,11 @@ train a neural network model.
 """
 
 import sys
-import time
-import random
 import argparse
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.callbacks import TensorBoard
 from fast5fetch.fast5data import train_test_val_split
-from fast5fetch.fast5data import data_and_label_generation
 from fast5fetch.fast5data import xy_generator_many
 from fast5fetch.fast5data import xy_generator_many_wrapper
 
@@ -44,10 +40,6 @@ def parse_args(args):
             help="tensorboard log directory")
     parser.add_argument("-g", "--gpulim", type=int, default=4096,
             help="GPU memory limit in mb, default is 4096")
-    parser.add_argument("--store_csv", type=str,
-            help="give directory to build datasets and store in csv")
-    parser.add_argument("--from_csv", type=str,
-            help="give directory to build datasets from csv for training")
     parser.add_argument("-c", "--cache", action="store_true",
             help="use caching to speed up training")
     return parser.parse_args(args)
@@ -66,7 +58,8 @@ def main():
     if gpus:
         limit_gpu(gpus[0], args.gpulim)
 
-    model = tf.keras.models.load_model(args.model)
+    modelpath = args.model
+    model = tf.keras.models.load_model(modelpath)
 
     pos_train_list, pos_test_list, pos_val_list = train_test_val_split(args.posdirs)
     neg_train_list, neg_test_list, neg_val_list = train_test_val_split(args.negdirs)
@@ -84,14 +77,14 @@ def main():
     neg_val_ds = make_class_ds(
             neg_val_list, 0, args.window, 1 - args.ratio, args.valreads, CPUS)
 
-    train_ds = build_full_ds(pos_train_ds, neg_train_ds, args.batchsize, args.ratio, max_epochs, args.cache)
-    test_ds = build_full_ds(pos_test_ds, neg_test_ds, args.batchsize, args.ratio, max_epochs, args.cache)
-    val_ds = build_full_ds(pos_val_ds, neg_val_ds, args.batchsize, args.ratio, max_epochs, args.cache)
+    train_ds = build_full_ds(pos_train_ds, neg_train_ds, args.batchsize,
+            args.ratio, max_epochs, args.cache)
+    val_ds = build_full_ds(pos_val_ds, neg_val_ds, args.batchsize,
+            args.ratio, max_epochs, args.cache)
 
     # Train the model
-    train_model(train_ds, val_ds, model, epoch_steps, val_steps, max_epochs, args.logs)
+    train_model(train_ds, val_ds, model, epoch_steps, val_steps, modelpath, max_epochs, args.logs)
 
-    # test_preds = testing(test_dataset, model, args.threshold)
 
 def limit_gpu(gpu_id, gpu_mem_lim):
     try:
@@ -113,36 +106,37 @@ def make_class_ds(file_list, label, window, ratio, numreads, CPUS):
     return ds 
 
 def build_full_ds(pos_ds, neg_ds, batchsize, ratio, max_epochs, cache):
-    #max_epochs = 60
-    ds =  tf.data.experimental.sample_from_datasets([pos_ds, neg_ds], weights=[ratio, (1 - ratio)], stop_on_empty_dataset=True)
+    ds =  tf.data.experimental.sample_from_datasets(
+            [pos_ds, neg_ds], weights=[ratio, (1 - ratio)],
+            stop_on_empty_dataset=True)
+    # ds = ds.repeat(5)
     if cache:
-        ds= ds.cache("neuralnets/src/cache_data/cache_data.file")
+        ds= ds.cache()
     ds = ds.batch(batchsize)
-    # ds = ds.repeat(max_epochs)
+    ds = ds.repeat(max_epochs)
     return ds
 
-def train_model(train_dataset, val_dataset, model, epoch_steps, val_steps, max_epochs, logs):
-    #epochs = 60
+def train_model(train_dataset, val_dataset, model, epoch_steps, val_steps, modelpath, max_epochs, logs):
 
     model.compile(
-            optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005),
+            optimizer = tf.keras.optimizers.Adam(learning_rate=0.001), # 0.0005),
             loss = 'binary_crossentropy',
             metrics = ["accuracy"],
             )
 
     callbacks = [
-            keras.callbacks.EarlyStopping(
-                monitor="val_loss",
-                min_delta=1e-2,
-                patience=5,
-                verbose=1
-                ),
-            # keras.callbacks.ModelCheckpoint(
-            #     filepath = model,
-            #     save_best_only=True,
+            # keras.callbacks.EarlyStopping(
             #     monitor="val_loss",
-            #     verbose=1,
+            #     min_delta=1e-2,
+            #     patience=5,
+            #     verbose=1
             #     ),
+            keras.callbacks.ModelCheckpoint(
+                filepath = modelpath,
+                save_best_only=True,
+                monitor="val_loss",
+                verbose=1,
+                ),
             # keras.callbacks.ReduceLROnPlateau(
             #     monitor="val_loss",
             #     factor=0.5,
@@ -157,55 +151,14 @@ def train_model(train_dataset, val_dataset, model, epoch_steps, val_steps, max_e
     fit = model.fit(
             train_dataset,
             epochs=max_epochs,
-            # steps_per_epoch = epoch_steps,
-            # callbacks=callbacks,
+            steps_per_epoch = epoch_steps,
+            callbacks=callbacks,
             validation_data=val_dataset,
-            # validation_steps = val_steps,
+            validation_steps = val_steps,
             )
+    # model.save("neuralnets/mycnn/models/test_save/")
 
     return fit
 
-
-def testing(test_dataset, model, threshold):
-    loss, acc = model.evaluate(test_dataset)
-    predictions= model.predict(test_dataset)
-    preds = (predictions >= threshold.astype("int32"))
-    # preds.tolist()
-    return preds
-
 if __name__ == "__main__":
     main()
-
-
-
-# train_list, val_list = train_val_split(args.posdirs)
-# gen = data_and_label_generation(val_list, 1)
-# for i in gen:
-#     print(i)
-
-# def test_generator(g):
-#     counter = 0
-#     for i in g:
-#         counter += 1
-#         print(i)
-#         if counter > 10:
-#             break
-
-# counter = 0
-# for x,y in train_dataset:
-#     counter += 1
-#     tf.print(list(y))
-#     # print("\t".join([*str(y)]))
-#     #tf.print(y.numpy())
-#     
-#     # rawdata = [str(num for num in list(y))]
-#     # print("\t".join([*rawdata]))
-#     # returns <generator object <genexpr> at blah>
-#     if counter == 33:
-#         break
-# counter = 0
-# for i in range(test_preds):
-#     counter += 1
-#     print(i)
-#     if counter > 10:
-#         break
